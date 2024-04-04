442dc8e009333996c5f84d0702034e7b79f72506
soyeunz <choso2261@gmail.com>
2024-04-04 09:26:34 +0900

    front_back 정리 초기세팅

---
+++ coffee_vue/backend/README.md
@@ -0,0 +1,17 @@
+#프로젝트시작
+npm init  #계속 엔터
+
+
+#dependency 설치
+npm install [dependency이름]
+
+- express : 백엔드 프레임워크
+- cors : cors 설정
+- json : json 파싱
+- body-parser : 요청 body 받기
+- nodemon : index.js 저장시 서버 새로고침
+
+
+# api 테스트
+- postman
+- REST Client (VS code extension)
--- coffee_vue/backend/index.js
+++ coffee_vue/backend/index.js
@@ -1,19 +1,63 @@
-const express = require("express");
+import express from "express";
+import oracledb from "oracledb";
+
+// Express 애플리케이션 생성
 const app = express();
 const port = 8081;
-const database = require("./database");
 
-app.get("/list", (req, res) => {
-  database.query("SELECT * FROM VUE_MENU", (err, data) => {
-    if (err) {
-      console.error("Error executing query:", err);
-      res.status(500).send("Internal Server Error");
-    } else {
-      res.status(200).json(data);
-    }
-  });
+// json 데이터 받기 위해
+app.use(express.json());
+
+app.listen(port, ()=>{
+    console.log('서버실행 port:',port);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+
+//DB연결부분
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+oracledb.initOracleClient({libDir:'D:/instantclient_21_13'})
+
+async function fun() {
+    let con;
+    try {
+        con = await oracledb.getConnection({
+            user: "soy2",
+            password: "1234",
+            connectString: "localhost:1521/xe"
+        });
+        const result = await con.execute(`SELECT * FROM VUE_MENU`);
+        return result;
+    } catch (err) {
+        throw err;
+    } finally {
+        if (con) {
+            try {
+                await con.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+}
+
+
+
+
+
+// 기본 주소 요청 핸들러
+app.get("/", (req, res) => {
+  console.log("기본주소 요청");
+  res.send("기본주소 요청");
+});
+
+
+// 목록 보이기
+app.get("/list", async (req, res) => {
+    try {
+        const data = await fun();
+        res.json(data.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("서버 오류");
+    }
 });
